47e8b97dd4f0b3b52c901fc2a13888bb3924e6ec
Shivam Adke <shivamadke@example.com>
2025-05-17 11:46:25 -0400

    Implemented Main Menu, Pause Menu, Settings panel, and volume sliders (Master, Music, SFX)

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public Slider masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    void Start()
+    {
+        // Load saved volume levels if needed
+        float volume;
+        if (audioMixer.GetFloat("MasterVolume", out volume))
+            masterSlider.value = Mathf.Pow(10f, volume / 20f);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(volume) * 20);
+    }
+}
